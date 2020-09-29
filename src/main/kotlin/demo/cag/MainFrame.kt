17ce2bf5d4a907e88e5918d921956585363b1724
Jean d'Arc <jean.darc@thoughtworks.com>
2020-09-29 13:46:21 +0100

    Using JPanel instead of JToolbar

--- src/main/kotlin/demo/cag/MainFrame.kt
+++ src/main/kotlin/demo/cag/MainFrame.kt
@@ -13,7 +13,7 @@ class MainFrame : JFrame("Constructive Area Geometry") {
     init {
         defaultCloseOperation = EXIT_ON_CLOSE
 
-        add(buildToolbar(), BorderLayout.NORTH)
+        add(buildToolbar(), BorderLayout.PAGE_START)
         add(editorPanel, BorderLayout.CENTER)
         pack()
         setLocationRelativeTo(null)
@@ -25,75 +25,90 @@ class MainFrame : JFrame("Constructive Area Geometry") {
         val shapesColor = Color(32, 230, 32)
         val operationsColor = Color(32, 230, 230)
         val warningColor = Color(230, 32, 32)
-        val insets = Insets(4, 4, 4, 4)
+        val insets = Insets(4, 4, 4, 8)
 
-        val toolbar = JToolBar()
-        toolbar.isFloatable = false
+        val toolbar = JPanel()
+        toolbar.layout = BoxLayout(toolbar, BoxLayout.X_AXIS)
 
-        val editButton = createToolbarButton("Edit", Icons.edit(interactionsColor), KeyEvent.VK_E, insets)
+        val editButton = createToggleButton("Manipulate", Icons.edit(interactionsColor), KeyEvent.VK_E, insets)
         editButton.toolTipText = "Move, rotate, select or deselect shapes"
         editButton.addActionListener { editorPanel.editMode() }
 
-        val triangleButton = createToolbarButton("Triangle", Icons.triangle(shapesColor), KeyEvent.VK_1, insets)
+        val triangleButton = createToggleButton("Triangle", Icons.triangle(shapesColor), KeyEvent.VK_1, insets)
         triangleButton.toolTipText = "Create a triangle shape"
         triangleButton.addActionListener { editorPanel.shapeMode(Shapes.TRIANGLE) }
 
-        val squareButton = createToolbarButton("Square", Icons.square(shapesColor), KeyEvent.VK_2, insets)
+        val squareButton = createToggleButton("Square", Icons.square(shapesColor), KeyEvent.VK_2, insets)
         squareButton.toolTipText = "Create a square shape"
         squareButton.addActionListener { editorPanel.shapeMode(Shapes.SQUARE) }
 
-        val rectangleButton = createToolbarButton("Rectangle", Icons.rectangle(shapesColor), KeyEvent.VK_3, insets)
+        val rectangleButton = createToggleButton("Rectangle", Icons.rectangle(shapesColor), KeyEvent.VK_3, insets)
         rectangleButton.toolTipText = "Create a rectangle shape"
         rectangleButton.addActionListener { editorPanel.shapeMode(Shapes.RECTANGLE) }
 
-        val pentagonButton = createToolbarButton("Pentagon", Icons.pentagon(shapesColor), KeyEvent.VK_4, insets)
+        val pentagonButton = createToggleButton("Pentagon", Icons.pentagon(shapesColor), KeyEvent.VK_4, insets)
         pentagonButton.toolTipText = "Create a pentagon shape"
         pentagonButton.addActionListener { editorPanel.shapeMode(Shapes.PENTAGON) }
 
-        val circleButton = createToolbarButton("Circle", Icons.circle(shapesColor), KeyEvent.VK_5, insets)
+        val circleButton = createToggleButton("Circle", Icons.circle(shapesColor), KeyEvent.VK_5, insets)
         circleButton.toolTipText = "Create a circle shape"
         circleButton.addActionListener { editorPanel.shapeMode(Shapes.CIRCLE) }
 
-        val capsuleButton = createToolbarButton("Capsule", Icons.capsule(shapesColor), KeyEvent.VK_6, insets)
+        val capsuleButton = createToggleButton("Capsule", Icons.capsule(shapesColor), KeyEvent.VK_6, insets)
         capsuleButton.toolTipText = "Create a capsule shape"
         capsuleButton.addActionListener { editorPanel.shapeMode(Shapes.CAPSULE) }
 
-        val unionButton = createToolbarButton("Union", Icons.union(operationsColor), KeyEvent.VK_0, insets)
+        val unionButton = createButton("Union", Icons.union(operationsColor), KeyEvent.VK_0, insets)
         unionButton.toolTipText = "Outputs the result of merging two shapes together"
         unionButton.addActionListener { editorPanel.addShapes() }
 
-        val differenceButton = createToolbarButton("Difference", Icons.difference(operationsColor), KeyEvent.VK_MINUS, insets)
+        val differenceButton = createButton("Difference", Icons.difference(operationsColor), KeyEvent.VK_MINUS, insets)
         differenceButton.toolTipText = "Outputs the result of cutting one shape out of another"
         differenceButton.addActionListener { editorPanel.subtractShapes() }
 
-        val intersectButton = createToolbarButton("Intersect", Icons.intersect(operationsColor), KeyEvent.VK_EQUALS, insets)
+        val intersectButton = createButton("Intersect", Icons.intersect(operationsColor), KeyEvent.VK_EQUALS, insets)
         intersectButton.toolTipText = "Outputs the result of intersecting two shape"
         intersectButton.addActionListener { editorPanel.intersectShapes() }
 
-        val deleteButton = createToolbarButton("Delete", Icons.delete(warningColor), KeyEvent.VK_DELETE, insets)
+        val deleteButton = createButton("Delete", Icons.delete(warningColor), KeyEvent.VK_DELETE, insets)
         deleteButton.toolTipText = "Delete selected shapes"
         deleteButton.addActionListener { editorPanel.deleteSelected() }
 
+        val buttonGroup = ButtonGroup()
+        buttonGroup.add(editButton)
+        buttonGroup.add(triangleButton)
+        buttonGroup.add(squareButton)
+        buttonGroup.add(rectangleButton)
+        buttonGroup.add(pentagonButton)
+        buttonGroup.add(circleButton)
+        buttonGroup.add(capsuleButton)
+
+        toolbar.add(Box.createRigidArea(Dimension(8, 0)))
         toolbar.add(editButton)
-        toolbar.add(MySeparator())
+        toolbar.add(Box.createHorizontalGlue())
         toolbar.add(triangleButton)
         toolbar.add(squareButton)
         toolbar.add(rectangleButton)
         toolbar.add(pentagonButton)
         toolbar.add(circleButton)
         toolbar.add(capsuleButton)
-        toolbar.add(MySeparator())
+        toolbar.add(Box.createHorizontalGlue())
         toolbar.add(unionButton)
         toolbar.add(differenceButton)
         toolbar.add(intersectButton)
-        toolbar.add(MySeparator())
+        toolbar.add(Box.createHorizontalGlue())
         toolbar.add(deleteButton)
-
+        toolbar.add(Box.createRigidArea(Dimension(8, 0)))
         return toolbar
     }
 
-    private fun createToolbarButton(label: String, iconImage: BufferedImage, mnemonic: Int, insets: Insets): JButton {
-        val button = JButton(label, ImageIcon(iconImage))
+    private fun createButton(label: String, iconImage: BufferedImage, mnemonic: Int, insets: Insets): AbstractButton =
+        embellishButton(JButton(label, ImageIcon(iconImage)), mnemonic, insets)
+
+    private fun createToggleButton(label: String, iconImage: BufferedImage, mnemonic: Int, insets: Insets): AbstractButton =
+        embellishButton(JToggleButton(label, ImageIcon(iconImage)), mnemonic, insets)
+
+    private fun embellishButton(button: AbstractButton, mnemonic: Int, insets: Insets): AbstractButton {
         button.mnemonic = mnemonic
         button.margin = insets
         return button
